6ef20e96ee8838de2850d4d486a6f99e4efed4e1
Manshi Gohil <manshigohil022@gmail.com>
2026-07-02 06:44:45 -0400

    Completed IBM Express Book Review final project

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,3 +1,4 @@
+const axios = require("axios");
 const express = require('express');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
@@ -6,38 +7,130 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  
+
+  const username = req.body.username;
+    const password = req.body.password;
+
+    if (username && password) {
+
+        if (!isValid(username)) {
+            users.push({ username, password });
+            return res.status(200).json({ message: "User successfully registered. Now you can login" });
+        } else {
+            return res.status(404).json({ message: "User already exists!" });
+        }
+
+    }
+
+    return res.status(404).json({ message: "Unable to register user." });
+});
+
+public_users.get('/asyncbooks', function (req, res) {
+
+    axios.get('http://localhost:5000/')
+        .then((response) => {
+            return res.status(200).json(response.data);
+        })
+        .catch((error) => {
+            return res.status(500).json({
+                message: "Error fetching books"
+            });
+        });
+
+});
+
+public_users.get('/asyncbooks/isbn/:isbn', function (req, res) {
+
+    const isbn = req.params.isbn;
+
+    axios.get(`http://localhost:5000/isbn/${isbn}`)
+        .then((response) => {
+            return res.status(200).json(response.data);
+        })
+        .catch((error) => {
+            return res.status(500).json({
+                message: "Error fetching book"
+            });
+        });
+
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+   return res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+ const isbn = req.params.isbn;
+    return res.json(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+    const booksByAuthor = {};
+
+    Object.keys(books).forEach((key) => {
+        if (books[key].author === author) {
+            booksByAuthor[key] = books[key];
+        }
+    });
+
+    return res.json(booksByAuthor);
+});
+
+public_users.get('/asyncbooks/author/:author', function (req, res) {
+
+    const author = req.params.author;
+
+    axios.get(`http://localhost:5000/author/${author}`)
+        .then((response) => {
+            return res.status(200).json(response.data);
+        })
+        .catch((error) => {
+            return res.status(500).json({
+                message: "Error fetching books by author"
+            });
+        });
+
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+    const booksByTitle = {};
+
+    Object.keys(books).forEach((key) => {
+        if (books[key].title === title) {
+            booksByTitle[key] = books[key];
+        }
+    });
+
+    return res.json(booksByTitle);
+});
+
+public_users.get('/asyncbooks/title/:title', function (req, res) {
+
+    const title = req.params.title;
+
+    axios.get(`http://localhost:5000/title/${title}`)
+        .then((response) => {
+            return res.status(200).json(response.data);
+        })
+        .catch((error) => {
+            return res.status(500).json({
+                message: "Error fetching books by title"
+            });
+        });
+
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+    return res.json(books[isbn].reviews);
 });
 
 module.exports.general = public_users;
